0d7a09816d3ad555e05187a8bcf7d508200b9c7d
mirek <github@mirekgab.pl>
2022-08-12 07:53:17 +0200

    initial commit

---
+++ src/main/java/pl/mirekgab/test_backend/StartController.java
@@ -0,0 +1,18 @@
+package pl.mirekgab.test_backend;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StartController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "hello";
+    }
+
+    @GetMapping("/mytext")
+    public String mytext() {
+        return "this is my text";
+    }
+}
