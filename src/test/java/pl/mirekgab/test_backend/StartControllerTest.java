18809ea62c8901f224067b326f4c51b2de753acb
Miroslaw Gabrus <poczta@mirekgab.pl>
2022-08-17 17:40:11 +0200

    add tests

---
+++ src/test/java/pl/mirekgab/test_backend/StartControllerTest.java
@@ -0,0 +1,36 @@
+package pl.mirekgab.test_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.equalTo;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+@AutoConfigureMockMvc
+class StartControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    void hello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/")
+                .accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("hello world from version 0.2.0")));
+    }
+
+    @Test
+    void mytext() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/mytext").accept(MediaType.ALL))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("this is my text")));
+    }
+}
