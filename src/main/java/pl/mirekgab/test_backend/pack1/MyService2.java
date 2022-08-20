57ac9cca20cb5db99d4da5672bbdb621147c9239
Miroslaw Gabrus <poczta@mirekgab.pl>
2022-08-20 20:06:00 +0200

    service2

---
+++ src/main/java/pl/mirekgab/test_backend/pack1/MyService2.java
@@ -0,0 +1,10 @@
+package pl.mirekgab.test_backend.pack1;
+
+public class MyService2 {
+
+    public int sum(int a, int b) {
+        return a+b;
+    }
+
+    public int multiply(int a, int b) {return a*b;}
+}
